dda0ce4b24a975f4adc07d7b3238d106ad93abdc
Abdul <kukluu@hotmail.com>
2021-07-11 16:31:54 -0700

    create react app with redux template | styled components Header styling inserted

---
+++ src/components/Header.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+  return (
+    <Nav>
+      <Logo src="/images/logo.svg" />
+
+      <NavMenu>
+        <a>
+          <img src="/images/home-icon.svg" alt="" />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src="/images/search-icon.svg" alt="" />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src="/images/watchlist-icon.svg" alt="" />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src="/images/original-icon.svg" alt="" />
+          <span>ORIGINALS</span>
+        </a>
+        <a>
+          <img src="/images/movie-icon.svg" alt="" />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src="/images/series-icon.svg" alt="" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+      <UserImg src="/images/userimg.jpeg" />
+    </Nav>
+  );
+};
+
+const Nav = styled.nav`
+  height: 70px;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+  background: #090b13;
+`;
+const Logo = styled.img`
+  width: 80px;
+  cursor: pointer;
+`;
+const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin: 0 0 0 25px;
+  align-items: center;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    // margin: 0 5px;
+    cursor: pointer;
+
+    img {
+      height: 20px;
+    }
+
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      position: relative;
+
+      &:after {
+        content: "";
+        height: 2px;
+        background: #ffffff;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        opacity: 0;
+        transform: scaleX(0);
+        transform-origin: left center;
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+      }
+    }
+
+    &:hover {
+      span:after {
+        transform: scaleX(1);
+        opacity: 1;
+      }
+    }
+  }
+`;
+const UserImg = styled.img`
+  border-radius: 50%;
+  cursor: pointer;
+  width: 48px;
+  height: 48px;
+`;
+
+export { Header };
